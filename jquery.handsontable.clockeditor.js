b171a751d729c8d945d18889bdb6035f8fd5a6fe
raduvultur <raduvultur@gmail.com>
2014-06-19 11:53:46 +0200

    Update jquery.handsontable.clockeditor.js
    No more autoclose after selecting minutes

--- jquery.handsontable.clockeditor.js
+++ jquery.handsontable.clockeditor.js
@@ -1,43 +1,52 @@
-var TimeEditor = Handsontable.editors.BaseEditor.prototype.extend();
+(function(Handsontable){
+	var TimeEditor = Handsontable.editors.BaseEditor.prototype.extend();
 
-TimeEditor.prototype.init = function(){
-    //Create clock input and update relevant properties
-    this.clock = $('<input>');
-    this.clock.addClass('clockInput').hide();
-	this.instance.rootElement.append(this.clock);	
-	this.clockInput = $('.clockInput').clockpicker({
-		placement: 'bottom',
-		align: 'left',
-		autoclose: true
-	});
-};	
+	TimeEditor.prototype.init = function(){
+		//Create clock editor and update relevant properties
+		this.clock = $('<input>');
+		this.clock.addClass('clockInput').hide();
+		this.instance.rootElement.append(this.clock);	
+		this.clockInput = $('.clockInput').clockpicker({
+			placement: 'bottom',
+			align: 'left',
+			autoclose: false,
+			donetext: 'Done'
+		});
+	};	
 
-TimeEditor.prototype.open = function () {
-    //make sure that input position matches cell position
-	var $td = $(this.TD);
-    var offset = $td.offset();
+	TimeEditor.prototype.open = function () {
+		//make sure that editor position matches cell position
+		var $td = $(this.TD);
+		var offset = $td.offset();
+		
+		this.clockInput.clockpicker('show');
+		
+		//remove clockpicker event handlers
+		$(document).off('click.clockpicker.cp1 focusin.clockpicker.cp1');
+		
+		var $cpop = $('.clockpicker-popover');
+		$cpop.offset({ top: offset.top + $td.height() + 10, left: offset.left });
+	 
+		$('.clockpicker-hours, .clockpicker-span-hours, .clockpicker-span-minutes').on('mousedown mouseup', function (event) {
+		  event.stopPropagation();
+		});
+		
+		$('.clockpicker-minutes').on('mousedown', function (event) {
+		  event.stopPropagation();
+		});
+	};
+
+	TimeEditor.prototype.close = function () {
+		this.clockInput.clockpicker('hide');
+	};
+	TimeEditor.prototype.getValue = function(){
+		return $('.clockpicker-span-hours').text() + ':' + $('.clockpicker-span-minutes').text();
+	};
+	TimeEditor.prototype.setValue = function(newValue){
+		this.clock.val(newValue);
+	}; 
+	TimeEditor.prototype.focus = function () {};
 	
-	this.clockInput.clockpicker('show');
-	//remove hiding event of clockpicker
-	$(document).off('click.clockpicker.cp1 focusin.clockpicker.cp1');
-	
-	var $cpop = $('.clockpicker-popover');
-	$cpop.offset({ top: offset.top + $td.height() + 10, left: offset.left });
- 
-	$('.clockpicker-hours, .clockpicker-span-hours, .clockpicker-span-minutes').on('mousedown mouseup', function (event) {
-      event.stopPropagation();
-    });	
-};
-
-TimeEditor.prototype.close = function () {
-    this.clockInput.clockpicker('hide');
-};
-TimeEditor.prototype.getValue = function(){
-	return $('.clockpicker-span-hours').text() + ':' + $('.clockpicker-span-minutes').text();
-};
-TimeEditor.prototype.setValue = function(newValue){
-	this.clock.val(newValue);
-}; 
-TimeEditor.prototype.focus = function () {};
-
-Handsontable.editors.registerEditor('time', TimeEditor);
+	Handsontable.editors.TimeEditor = TimeEditor;
+	Handsontable.editors.registerEditor('time', TimeEditor);
+}(Handsontable));
